185c483b500de9092ef3429fee0eb4e9d301001d
Eduardo França <edox86@gmail.com>
2023-10-04 18:59:52 -0300

    more db questions

--- slides/salesiano-db1-2023/quiz/app.js
+++ slides/salesiano-db1-2023/quiz/app.js
@@ -36,7 +36,7 @@ Vue.component('quiz-app', {
         <div v-if=" Math.round((score / questions.length) * 100) < 100 && Math.round((score / questions.length) * 100) > 0">
         <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/VLzEYLMhEAlUydp4uW" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>        
         </div>
-        
+
         <div v-if=" Math.round((score / questions.length) * 100) === 0 ">
         <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/jZnOF0UoupsvKT0gKl" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>        
         </div>
@@ -71,66 +71,451 @@ Vue.component('quiz-app', {
     },
     questions() {
       // Define your questions and answer options here
-      const questions = [
-        {
-          questionText: `<p>What does SQL stand for?</p> <img src="https://devtools.com.br/blog/wp-content/uploads/2013/06/MySQL-Logo.wine_-2048x1365.png">`,
-          answerOptions: [
-            { answerText: "Standard Query Language", isCorrect: false },
-            { answerText: "Structured Query Language", isCorrect: true },
-            { answerText: "System Query Language", isCorrect: false },
-            { answerText: "Server Query Language", isCorrect: false },
-          ],
-        },
-        {
-          questionText: "Which SQL statement is used to retrieve data from a database?",
-          answerOptions: [
-            { answerText: "GET", isCorrect: false },
-            { answerText: "SELECT", isCorrect: true },
-            { answerText: "FETCH", isCorrect: false },
-            { answerText: "QUERY", isCorrect: false },
-          ],
-        },
-        {
-          questionText: "In SQL, which clause is used to filter the result set based on a specified condition?",
-          answerOptions: [
-            { answerText: "FROM", isCorrect: false },
-            { answerText: "WHERE", isCorrect: true },
-            { answerText: "HAVING", isCorrect: false },
-            { answerText: "GROUP BY", isCorrect: false },
-          ],
-        },
-      ]
-
-      questions.forEach((question) => {
-        question.answerOptions = this.shuffleArray(question.answerOptions)
-      })
-
-      return questions;
-    },
+const questions = [
+  {
+    questionText:
+      "Qual cláusula SQL é usada para filtrar registros em uma consulta?",
+    answerOptions: [
+      { answerText: "SELECT", isCorrect: false },
+      { answerText: "FROM", isCorrect: false },
+      { answerText: "WHERE", isCorrect: true },
+      { answerText: "ORDER BY", isCorrect: false },
+    ],
   },
-  methods: {
-    handleAnswerButtonClick(isCorrect) {
-      if (isCorrect) {
-        this.$store.commit('incrementScore');
-      }
-      this.$store.commit('nextQuestion');
-      if (this.currentQuestion === this.questions.length) {
-        this.$store.commit('setShowScore', true);
-      }
-    },
-    restartQuiz() {
-      this.$store.commit('resetQuiz');
-    },
-    shuffleArray(array) {
-      // Shuffling algorithm (Fisher-Yates Shuffle)
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
-      }
-      return array;
-    },
+  {
+    questionText:
+      "Para ordenar os resultados de uma consulta SQL em ordem ascendente, qual cláusula você usaria?",
+    answerOptions: [
+      { answerText: "SELECT", isCorrect: false },
+      { answerText: "FROM", isCorrect: false },
+      { answerText: "ORDER BY", isCorrect: true },
+      { answerText: "WHERE", isCorrect: false },
+    ],
   },
-})
+  {
+    questionText:
+      "Qual cláusula SQL é usada para limitar o número de resultados retornados por uma consulta?",
+    answerOptions: [
+      { answerText: "SELECT", isCorrect: false },
+      { answerText: "FROM", isCorrect: false },
+      { answerText: "LIMIT", isCorrect: true },
+      { answerText: "WHERE", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "Para realizar uma pesquisa de padrão em uma coluna de texto em SQL, qual operador você usaria?",
+    answerOptions: [
+      { answerText: "=", isCorrect: false },
+      { answerText: "<>", isCorrect: false },
+      { answerText: "LIKE", isCorrect: true },
+      { answerText: "IN", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "Qual cláusula SQL é usada para especificar quais colunas devem ser incluídas no resultado de uma consulta?",
+    answerOptions: [
+      { answerText: "SELECT", isCorrect: true },
+      { answerText: "FROM", isCorrect: false },
+      { answerText: "WHERE", isCorrect: false },
+      { answerText: "GROUP BY", isCorrect: false },
+    ],
+  },
+
+  {
+    questionText:
+      "Qual é o principal objetivo da primeira forma normal (1NF) em bancos de dados?",
+    answerOptions: [
+      { answerText: "Eliminar a duplicação de dados", isCorrect: true },
+      { answerText: "Garantir a integridade referencial", isCorrect: false },
+      {
+        answerText: "Dividir a tabela em várias tabelas menores",
+        isCorrect: false,
+      },
+      {
+        answerText: "Garantir que cada coluna tenha um valor único",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questionText:
+      "Qual é a regra principal da segunda forma normal (2NF) em bancos de dados?",
+    answerOptions: [
+      {
+        answerText: "Cada coluna deve conter apenas valores únicos",
+        isCorrect: false,
+      },
+      {
+        answerText: "Cada coluna deve ser do mesmo tipo de dados",
+        isCorrect: false,
+      },
+      {
+        answerText: "Toda coluna deve depender da chave primária completa",
+        isCorrect: true,
+      },
+      {
+        answerText: "A tabela deve ser dividida em duas tabelas menores",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questionText:
+      "Qual dos seguintes é um bom exemplo de uma dependência transitiva em um banco de dados?",
+    answerOptions: [
+      {
+        answerText:
+          "A relação entre um cliente e seu número de telefone, onde o número de telefone depende diretamente do cliente.",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "A relação entre um produto, seu preço e o código do fabricante, onde o preço depende do produto e o código do fabricante depende do preço.",
+        isCorrect: true,
+      },
+      {
+        answerText:
+          "A relação entre um aluno, sua nota em um exame e a data do exame, onde a nota depende do aluno e a data do exame depende da nota.",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "A relação entre um funcionário, seu endereço residencial e o número do departamento em que trabalha, onde o endereço depende do funcionário e o número do departamento depende do endereço residencial.",
+        isCorrect: false,
+      },
+    ],
+  },
+
+  {
+    questionText:
+      "O que a terceira forma normal (3NF) em bancos de dados busca alcançar?",
+    answerOptions: [
+      { answerText: "Eliminar a duplicação de dados", isCorrect: false },
+      {
+        answerText: "Garantir que cada coluna tenha um valor único",
+        isCorrect: false,
+      },
+      {
+        answerText: "Reduzir o número de tabelas em um banco de dados",
+        isCorrect: false,
+      },
+      { answerText: "Eliminar dependências transitivas", isCorrect: true },
+    ],
+  },
+  {
+    questionText:
+      "Quais são os tipos de dados comuns que você pode usar em colunas de um banco de dados?",
+    answerOptions: [
+      {
+        answerText: "Números (integer e float), texto (char e varchar)",
+        isCorrect: true,
+      },
+      {
+        answerText: "Cores (RGB) e nomes de animais",
+        isCorrect: false,
+      },
+      {
+        answerText: "Cores (RGB) e tamanhos (px)",
+        isCorrect: false,
+      },
+      {
+        answerText: "Datas (YYYY-MM-DD) e horas (HH:MM:SS)",
+        isCorrect: false,
+      },
+    ],
+  },
+
+  {
+    questionText:
+      "O que é um Diagrama de Entidade-Relacionamento (DER) em bancos de dados?",
+    answerOptions: [
+      {
+        answerText:
+          "Um diagrama que mostra a relação entre frutas cítricas, como laranjas e bananas, em um banco de dados de receitas.",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "Um diagrama que representa visualmente as entidades, atributos e relacionamentos em um banco de dados.",
+        isCorrect: true,
+      },
+      {
+        answerText:
+          "Um diagrama que exibe a conexão de um banco de dados a um servidor web.",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "Um diagrama que ilustra a lógica de programação de um aplicativo de banco de dados.",
+        isCorrect: false,
+      },
+    ],
+  },
+
+  {
+    questionText:
+      "O que caracteriza uma relação um-para-um em um modelo de banco de dados?",
+    answerOptions: [
+      {
+        answerText:
+          "Cada registro na tabela A pode estar associado a apenas um registro na tabela B, e vice-versa.",
+        isCorrect: true,
+      },
+      {
+        answerText:
+          "Cada registro na tabela A pode estar associado a vários registros na tabela B, mas não o contrário.",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "Cada registro na tabela A pode estar associado a vários registros na tabela B, e vice-versa.",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "Não há relação entre as tabelas A e B em uma relação um-para-um.",
+        isCorrect: false,
+      },
+    ],
+  },
+
+  {
+    questionText:
+      "O que caracteriza uma relação um-para-muitos em um modelo de banco de dados?",
+    answerOptions: [
+      {
+        answerText:
+          "Cada registro na tabela A pode estar associado a vários registros na tabela B, mas cada registro na tabela B está associado a apenas um registro na tabela A.",
+        isCorrect: true,
+      },
+      {
+        answerText:
+          "Cada registro na tabela A pode estar associado a apenas um registro na tabela B, e vice-versa.",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "Cada registro na tabela A pode estar associado a vários registros na tabela B, e cada registro na tabela B pode estar associado a vários registros na tabela A.",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "Não há relação entre as tabelas A e B em uma relação um-para-muitos.",
+        isCorrect: false,
+      },
+    ],
+  },
+
+  {
+    questionText:
+      "O que caracteriza uma relação muitos-para-muitos em um modelo de banco de dados?",
+    answerOptions: [
+      {
+        answerText:
+          "Cada registro na tabela A pode estar associado a vários registros na tabela B, e vice-versa.",
+        isCorrect: true,
+      },
+      {
+        answerText:
+          "Cada registro na tabela A pode estar associado a apenas um registro na tabela B, e vice-versa.",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "Cada registro na tabela A pode estar associado a vários registros na tabela B, mas cada registro na tabela B está associado a apenas um registro na tabela A.",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "Não há relação entre as tabelas A e B em uma relação muitos-para-muitos.",
+        isCorrect: false,
+      },
+    ],
+  },
+
+  {
+    questionText: `
+onsidere a seguinte tabela de dados chamada 'Pessoas':
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Nome</th>
+    <th>Idade</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Maria</td>
+    <td>30</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>João</td>
+    <td>25</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Ana</td>
+    <td>35</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>Carlos</td>
+    <td>28</td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td>Beatriz</td>
+    <td>22</td>
+  </tr>
+</table>
+<br>
+Agora, observe a seguinte consulta SQL:<br>
+<pre>
+SELECT Nome
+FROM Pessoas
+WHERE Idade < 30;
+</pre>
+<br>
+Qual será o resultado desta consulta?
+`,
+    answerOptions: [
+      {
+        answerText: "Maria, João, Ana",
+        isCorrect: false,
+      },
+      {
+        answerText: "Carlos, Beatriz",
+        isCorrect: false,
+      },
+      {
+        answerText: "João, Carlos, Beatriz",
+        isCorrect: true,
+      },
+      {
+        answerText: "Maria, Ana",
+        isCorrect: false,
+      },
+    ],
+  },
+
+  {
+    questionText: `
+Considere duas tabelas em um banco de dados:
+
+Tabela "Pedidos":
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Produto</th>
+    <th>Quantidade</th>
+    <th>ID do Cliente</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Camiseta</td>
+    <td>2</td>
+    <td>101</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Tênis</td>
+    <td>1</td>
+    <td>102</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Jeans</td>
+    <td>3</td>
+    <td>103</td>
+  </tr>
+</table>
+
+Tabela "Clientes":
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Nome</th>
+    <th>Email</th>
+  </tr>
+  <tr>
+    <td>101</td>
+    <td>Maria</td>
+    <td>maria@email.com</td>
+  </tr>
+  <tr>
+    <td>102</td>
+    <td>João</td>
+    <td>joao@email.com</td>
+  </tr>
+  <tr>
+    <td>103</td>
+    <td>Ana</td>
+    <td>ana@email.com</td>
+  </tr>
+</table>
+<br>
+Agora, observe a seguinte consulta SQL que utiliza um JOIN entre as tabelas "Pedidos" e "Clientes":
+<pre>
+SELECT Pedidos.ID, Clientes.Nome
+FROM Pedidos
+INNER JOIN Clientes ON Pedidos.ID do Cliente = Clientes.ID;
+</pre> 
+<br>
+Quais resultados serão retornados como resultado dessa consulta?
+`,
+    answerOptions: [
+      {
+        answerText: "1 - Maria, 2 - João, 3 - Ana",
+        isCorrect: true,
+      },
+      {
+        answerText: "101 - Maria, 102 - João, 103 - Ana",
+        isCorrect: false,
+      },
+      {
+        answerText: "Camiseta - Maria, Tênis - João, Jeans - Ana",
+        isCorrect: false,
+      },
+      {
+        answerText: "Camiseta, Tênis, Jeans",
+        isCorrect: false,
+      },
+    ],
+  },
+];
+
+          questions.forEach((question) => {
+            question.answerOptions = this.shuffleArray(question.answerOptions)
+          })
+
+          return questions;
+        },
+    },
+    methods: {
+      handleAnswerButtonClick(isCorrect) {
+        if (isCorrect) {
+          this.$store.commit('incrementScore');
+        }
+        this.$store.commit('nextQuestion');
+        if (this.currentQuestion === this.questions.length) {
+          this.$store.commit('setShowScore', true);
+        }
+      },
+      restartQuiz() {
+        this.$store.commit('resetQuiz');
+      },
+      shuffleArray(array) {
+        // Shuffling algorithm (Fisher-Yates Shuffle)
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+        }
+        return array;
+      },
+    },
+  })
 
 new Vue({
   el: '#app',
